a7353bc36429aaf8083c0d7578794ede68ccd051
avniamitlumar <avni.shivi@gmail.com>
2023-01-05 23:55:09 +0530

    a2z ll

---
+++ pairsumll.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+        // code here
+        vector<pair<int,int>> my;
+        Node *j=head;
+        while(j->next)
+        {
+            j=j->next;
+        }
+        Node *i=head;
+        while(i!=j)
+        {
+            if(i->data+j->data==target)
+            {
+                pair<int,int>p;
+                p.first=i->data;
+                p.second=j->data;
+                i=i->next;
+                //j=j->prev;
+                my.push_back(p);
+            }
+            else if(i->data+j->data<target)
+            {
+                i=i->next;
+            }
+            else
+            {
+                j=j->prev;
+            }
+        }
+        return my;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, target;
+        cin >> target >> n;
+        int a;
+        cin >> a;
+        Node *head = new Node(a);
+        Node *tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> a;
+            tail->next = new Node(a);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+        Solution ob;
+        auto ans = ob.findPairsWithGivenSum(head, target);
+        if (ans.size() == 0)
+            cout << "-1";
+        else
+        {
+            for (int i = 0; i < ans.size(); i++)
+            {
+                cout << "(" << ans[i].first << "," << ans[i].second << ")"
+                     << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
